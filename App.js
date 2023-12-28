048e661108093b2a08458baf3ebd347b6338a4b1
Jordan <jordanwatson000@gmail.com>
2023-12-28 20:02:56 +0000

    developing the basic app and learning functionality

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
+  const [name, setName] = useState('Jordan')
+  const [gameChosen, setGameChosen] = useState('Mario')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldTitle}>GAMES</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Hello {name}</Text>
+        <Text>What would you like to play today?</Text>
+        <Text>You would like to play - {gameChosen}</Text>
+        <Text></Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="submit" />
+      </View>
+      <Text>Change name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="My name is"
+        onChangeText={(val) => {
+          setName(val)
+        }}
+      />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +40,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  header: {
+    backgroundColor: '#f5d57f',
+    padding: 50,
+  },
+  boldTitle: {
+    fontWeight: '400',
+  },
+  body: {
+    backgroundColor: '#faebc8',
+    padding: 20,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: 'grey',
+    minWidth: 100,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+})
